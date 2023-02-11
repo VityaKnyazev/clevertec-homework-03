8bddb0d5a18cb653404b26d14c42f5288c43e02d
Vitya Knyazev <Utopio@tut.by>
2023-02-11 22:56:16 +0300

    Added tasks till 14

--- src/main/java/ru/clevertec/knyazev/Main.java
+++ src/main/java/ru/clevertec/knyazev/Main.java
@@ -1,9 +1,12 @@
 package ru.clevertec.knyazev;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import ru.clevertec.knyazev.model.Animal;
 import ru.clevertec.knyazev.model.Car;
@@ -59,6 +62,7 @@ public class Main {
 	// начинаются на "A" и вывели их в консоли.
 	private static void task3() throws IOException {
 		List<Animal> animals = Util.getAnimals();
+		System.out.println("3. Filtered animals countries:");
 		animals.stream().filter(animal -> animal.getAge() >= 30).map(Animal::getOrigin).distinct()
 				.filter(country -> country.startsWith("A")).forEach(System.out::println);
 	}
@@ -99,29 +103,60 @@ public class Main {
 
 	private static void task9() throws IOException {
 		List<Animal> animals = Util.getAnimals();
-		int lengthOfShortestCharArrBreed = animals.stream().mapToInt(animal -> animal.getBread().toCharArray().length).sorted().findFirst().orElse(-1);
+		int lengthOfShortestCharArrBreed = animals.stream().mapToInt(animal -> animal.getBread().toCharArray().length)
+				.sorted().findFirst().orElse(-1);
 		System.out.println("9. The shortest char array breed length is " + lengthOfShortestCharArrBreed);
 	}
 
 	private static void task10() throws IOException {
 		List<Animal> animals = Util.getAnimals();
-		int sumOfAnimalsAges = animals.stream().map(animal -> animal.getAge()).reduce(0, (prevAge, currAge) -> prevAge + currAge);
+		int sumOfAnimalsAges = animals.stream().map(animal -> animal.getAge()).reduce(0,
+				(prevAge, currAge) -> prevAge + currAge);
 		System.out.println("10. Sum of animals ages is " + sumOfAnimalsAges);
 	}
 
 	private static void task11() throws IOException {
 		List<Animal> animals = Util.getAnimals();
-		// animals.stream() Продолжить ...
+		double averageAgeAnimalsFromIndenesian = animals.stream()
+				.filter(animal -> animal.getOrigin().equals(("Indonesian"))).mapToInt(animal -> animal.getAge())
+				.average().orElse(0.0d);
+		System.out.println("11. Average age animals from Indonesian is " + averageAgeAnimalsFromIndenesian);
 	}
 
 	private static void task12() throws IOException {
 		List<Person> people = Util.getPersons();
-//        Продолжить...
+		System.out.println("12. List of pure candidates in French legion:");
+		// 1996 - 2005
+		people.stream()
+				.filter(human -> (human.getGender().equals("Male"))
+						&& (human.getDateOfBirth().compareTo(LocalDate.now().minusYears(18)) <= 0)
+						&& (human.getDateOfBirth().compareTo(LocalDate.now().minusYears(27)) >= 0))
+				.sorted(Comparator.comparing(Person::getRecruitmentGroup)).limit(200).collect(Collectors.toList())
+				.forEach(System.out::println);
 	}
 
+	// 1 Из Hospital
+	// 2 Людей в возрасте до 18 и после 58 и 63
+	// 3 После всех остальных людей
+	// 4 Уникальные значения и ограничение (limit) 500
 	private static void task13() throws IOException {
 		List<House> houses = Util.getHouses();
-		// Продолжить...
+		/// 1
+		List<Person> hospitalPersons = houses.stream().filter(house -> house.getBuildingType().equals("Hospital"))
+				.flatMap(house -> house.getPersonList().stream()).collect(Collectors.toList());
+		// 2
+		List<Person> youngAndWizdomPersons = houses.stream().flatMap(house -> house.getPersonList().stream())
+				.filter(person -> (person.getDateOfBirth().compareTo(LocalDate.now().minusYears(18)) >= 0)
+						|| ((person.getDateOfBirth().compareTo(LocalDate.now().minusYears(58)) <= 0)
+								&& (person.getGender().equals("Female")))
+						|| ((person.getDateOfBirth().compareTo(LocalDate.now().minusYears(63)) <= 0)
+								&& person.getGender().equals("Male"))).collect(Collectors.toList());
+		// 3 the others
+		List<Person> theOtherPersons = houses.stream().flatMap(house -> house.getPersonList().stream()).collect(Collectors.toList());
+		
+		List<Person> evacuatedPersons = Stream.of(hospitalPersons, youngAndWizdomPersons, theOtherPersons).flatMap(personsList -> personsList.stream()).distinct().limit(500).collect(Collectors.toList());
+		System.out.println("13. List of evacuated Persons of a first queu:");
+		evacuatedPersons.forEach(System.out::println);
 	}
 
 	private static void task14() throws IOException {
