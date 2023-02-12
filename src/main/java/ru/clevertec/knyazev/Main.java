d57f3229450410ccf1337123652eb604283606d2
Vitya Knyazev <Utopio@tut.by>
2023-02-12 23:51:40 +0300

    Added solutions for 14, 15 tasks.

--- src/main/java/ru/clevertec/knyazev/Main.java
+++ src/main/java/ru/clevertec/knyazev/Main.java
@@ -2,7 +2,6 @@ package ru.clevertec.knyazev;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,16 +35,13 @@ public class Main {
 
 	private static void task1() throws IOException {
 		List<Animal> animals = Util.getAnimals();
-		// TODO сгрупировать по 7 ???
-		animals.stream().filter(animal -> ((animal.getAge() >= 20) && (animal.getAge() <= 30)))
+		// сгрупировать по 7 в одном стриме???
+		System.out.println("1. Director of Zoo № 3 keep 7 animals: ");
+		animals.stream().filter(animal -> ((animal.getAge() >= 10) && (animal.getAge() <= 20)))
 				.sorted((a1, a2) -> Integer.compare(a1.getAge(), a2.getAge())).skip(14).limit(7)
 				.forEach(System.out::println);
 	}
 
-	// Отфильтровали животных по происхождению из Японии (Japanese), преобразовали
-	// символы в происхождении каждого животного в верхний регистр
-	// и там, где пол животного женский (Female) выбираем строки происхождения и
-	// сводим к одной строке
 	private static void task2() throws IOException {
 		List<Animal> animals = Util.getAnimals();
 		String filteredAnimalsBreads = animals.stream().filter(animal -> animal.getOrigin().equals("Japanese"))
@@ -55,15 +51,10 @@ public class Main {
 		System.out.println("2. Filtered Animals Breads -" + filteredAnimalsBreads);
 	}
 
-	// Отфильтровали животных по возрасту старше 30 лет (включительно), выбрали
-	// строки с происхождением из каждого
-	// животного оставляя только уникальные значения, отфильтровали эти значения
-	// происхождения, оставив только те, которые
-	// начинаются на "A" и вывели их в консоли.
 	private static void task3() throws IOException {
 		List<Animal> animals = Util.getAnimals();
 		System.out.println("3. Filtered animals countries:");
-		animals.stream().filter(animal -> animal.getAge() >= 30).map(Animal::getOrigin).distinct()
+		animals.stream().filter(animal -> animal.getAge() > 30).map(Animal::getOrigin).distinct()
 				.filter(country -> country.startsWith("A")).forEach(System.out::println);
 	}
 
@@ -97,7 +88,7 @@ public class Main {
 	private static void task8() throws IOException {
 		List<Animal> animals = Util.getAnimals();
 		Animal filteredTheOldestAnimal = animals.stream().sorted((a1, a2) -> a1.getBread().compareTo(a2.getBread()))
-				.limit(100).sorted(Comparator.comparing(Animal::getAge).reversed()).findFirst().orElse(new Animal());
+				.limit(100).sorted(Comparator.comparing(Animal::getAge).reversed()).findFirst().orElse(null);
 		System.out.println("8. Filtered the oldest animal is " + filteredTheOldestAnimal);
 	}
 
@@ -126,11 +117,11 @@ public class Main {
 	private static void task12() throws IOException {
 		List<Person> people = Util.getPersons();
 		System.out.println("12. List of pure candidates in French legion:");
-		// 1996 - 2005
+		// ~ 1996 - 2005 | [18 27)
 		people.stream()
 				.filter(human -> (human.getGender().equals("Male"))
 						&& (human.getDateOfBirth().compareTo(LocalDate.now().minusYears(18)) <= 0)
-						&& (human.getDateOfBirth().compareTo(LocalDate.now().minusYears(27)) >= 0))
+						&& (human.getDateOfBirth().compareTo(LocalDate.now().minusYears(27)) > 0))
 				.sorted(Comparator.comparing(Person::getRecruitmentGroup)).limit(200).collect(Collectors.toList())
 				.forEach(System.out::println);
 	}
@@ -144,28 +135,115 @@ public class Main {
 		/// 1
 		List<Person> hospitalPersons = houses.stream().filter(house -> house.getBuildingType().equals("Hospital"))
 				.flatMap(house -> house.getPersonList().stream()).collect(Collectors.toList());
-		// 2
+		//  (18 58,63]
 		List<Person> youngAndWizdomPersons = houses.stream().flatMap(house -> house.getPersonList().stream())
-				.filter(person -> (person.getDateOfBirth().compareTo(LocalDate.now().minusYears(18)) >= 0)
+				.filter(person -> (person.getDateOfBirth().compareTo(LocalDate.now().minusYears(18)) > 0)
 						|| ((person.getDateOfBirth().compareTo(LocalDate.now().minusYears(58)) <= 0)
 								&& (person.getGender().equals("Female")))
 						|| ((person.getDateOfBirth().compareTo(LocalDate.now().minusYears(63)) <= 0)
-								&& person.getGender().equals("Male"))).collect(Collectors.toList());
+								&& person.getGender().equals("Male")))
+				.collect(Collectors.toList());
 		// 3 the others
-		List<Person> theOtherPersons = houses.stream().flatMap(house -> house.getPersonList().stream()).collect(Collectors.toList());
-		
-		List<Person> evacuatedPersons = Stream.of(hospitalPersons, youngAndWizdomPersons, theOtherPersons).flatMap(personsList -> personsList.stream()).distinct().limit(500).collect(Collectors.toList());
+		List<Person> theOtherPersons = houses.stream().flatMap(house -> house.getPersonList().stream())
+				.collect(Collectors.toList());
+
+		List<Person> evacuatedPersons = Stream.of(hospitalPersons, youngAndWizdomPersons, theOtherPersons)
+				.flatMap(personsList -> personsList.stream()).distinct().limit(500).collect(Collectors.toList());
 		System.out.println("13. List of evacuated Persons of a first queu:");
 		evacuatedPersons.forEach(System.out::println);
 	}
 
 	private static void task14() throws IOException {
 		List<Car> cars = Util.getCars();
-		// Продолжить...
+		System.out.println("14. Transport company logistic calculation task:");
+		// 1
+		List<Car> turkmenistanCars = cars.stream()
+				.filter(car -> car.getCarMake().equals("Jaguar") || car.getColor().equals("White")).toList();
+		// 2
+		// Трактовал так: Все авто с массой до 1500 кг (не включительно), а также марки
+		// BMW, Lexus, Chrysler, Toyota
+		List<Car> uzbekistanCars = cars.stream()
+				.filter(car -> (car.getMass() < 1500) || car.getCarMake().equals("BMW")
+						|| car.getCarMake().equals("Lexus") || car.getCarMake().equals("Chrysler")
+						|| car.getCarMake().equals("Toyota"))
+				.filter(car -> !turkmenistanCars.contains(car)).toList();
+		// 3
+		List<Car> kazahstanCars = cars.stream()
+				.filter(car -> (car.getColor().equals("Black") && (car.getMass() > 4000))
+						|| car.getCarMake().equals("GMC") || car.getCarMake().equals("Dodge"))
+				.filter(car -> !turkmenistanCars.contains(car) && !uzbekistanCars.contains(car)).toList();
+		// 4
+		List<Car> kurgustanCars = cars.stream()
+				.filter(car -> (car.getReleaseYear() < 1982) || car.getCarModel().equals("Civic")
+						|| car.getCarModel().equals("Cherokee"))
+				.filter(car -> !turkmenistanCars.contains(car) && !uzbekistanCars.contains(car)
+						&& !kazahstanCars.contains(car))
+				.toList();
+		// 5
+		List<Car> russianCars = cars.stream()
+				.filter(car -> (!car.getColor().equals("Yellow") && !car.getColor().equals("Red")
+						&& !car.getColor().equals("Green") && !car.getColor().equals("Blue")) || car.getPrice() > 40000)
+				.filter(car -> !turkmenistanCars.contains(car) && !uzbekistanCars.contains(car)
+						&& !kazahstanCars.contains(car) && !kurgustanCars.contains(car))
+				.toList();
+
+		// 6
+		List<Car> mongolianCars = cars.stream().filter(car -> car.getVin().contains("59"))
+				.filter(car -> !turkmenistanCars.contains(car) && !uzbekistanCars.contains(car)
+						&& !kazahstanCars.contains(car) && !kurgustanCars.contains(car) && !russianCars.contains(car))
+				.toList();
+
+		// 7 ~Трансппортные расходы и выручка
+		double turkmenistanLogisticCost = calculateLogisticCost(turkmenistanCars);
+		double uzbekistanLogisticCost = calculateLogisticCost(uzbekistanCars);
+		double kazahstanLogisticCost = calculateLogisticCost(kazahstanCars);
+		double kurgustanLogisticCost = calculateLogisticCost(kurgustanCars);
+		double russianLogisticCost = calculateLogisticCost(russianCars);
+		double mongolianLogisticCost = calculateLogisticCost(mongolianCars);
+
+		double totalLogisticCost = Stream
+				.of(turkmenistanLogisticCost, uzbekistanLogisticCost, kazahstanLogisticCost, kurgustanLogisticCost,
+						russianLogisticCost, mongolianLogisticCost)
+				.reduce(0.0d, (prevVal, currVal) -> prevVal + currVal);
+
+		double totalOutcome = Stream
+				.of(turkmenistanCars, uzbekistanCars, kazahstanCars, kurgustanCars, russianCars, mongolianCars)
+				.flatMap(carsList -> carsList.stream()).map(car -> car.getPrice())
+				.reduce(0, (prevPrice, currentPrice) -> prevPrice + currentPrice) - totalLogisticCost;
+
+		System.out.println("Total logistc cost per countries: " + System.lineSeparator() + "Turkmenistan: "
+				+ String.format("%-10.2f", turkmenistanLogisticCost) + " $" + System.lineSeparator() + "Uzbekistan: "
+				+ String.format("%-10.2f", uzbekistanLogisticCost) + " $" + System.lineSeparator() + "Kazahstan: "
+				+ String.format("%-10.2f", kazahstanLogisticCost) + " $" + System.lineSeparator() + "Kurgustan: "
+				+ String.format("%-10.2f", kurgustanLogisticCost) + " $" + System.lineSeparator() + "Russia: "
+				+ String.format("%-10.2f", russianLogisticCost) + " $" + System.lineSeparator() + "Mongolia: "
+				+ String.format("%-10.2f", mongolianLogisticCost) + " $" + System.lineSeparator());
+
+		System.out.println("Logistic company total outcome: " + String.format("%-10.2f", totalOutcome) + " $");
 	}
 
 	private static void task15() throws IOException {
 		List<Flower> flowers = Util.getFlowers();
-		// Продолжить...
+		
+		final int dayInFiveYears = 5 * 365;
+		final double waterPricePerСub = 1.39d;
+		
+		double flowersCost = flowers.stream()
+				.sorted(Comparator.comparing(Flower::getOrigin).reversed().thenComparing(Flower::getPrice)
+						.thenComparing(Flower::getWaterConsumptionPerDay).reversed())
+				.filter(flower -> flower.getCommonName().matches("^[CDEFGHIJKLMNOPQRS].{0,}"))
+				.filter(flower -> flower.isShadePreferred()
+						&& flower.getFlowerVaseMaterial().containsAll(List.of("Glass", "Aluminum", "Steel")))
+				.mapToDouble(flower -> flower.getPrice() + flower.getWaterConsumptionPerDay() * dayInFiveYears * waterPricePerСub)
+				.reduce(0.0d, (prevCost, currentCost) -> prevCost + currentCost);
+		
+		System.out.println("15. Budget cost on keeping filtered flowers is " + String.format("%10.2f", flowersCost) + " $");
+	}
+
+	private static final double calculateLogisticCost(List<Car> cars) {
+		final double logisticCostPerT = 7.14;
+
+		return cars.stream().map(car -> car.getMass()).reduce(0, (prevMass, currentMass) -> prevMass + currentMass)
+				* logisticCostPerT;
 	}
 }
